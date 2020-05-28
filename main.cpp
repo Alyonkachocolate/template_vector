a7a12d7db5db4b1d30d8d6c8a2495df6bc501076
Alyonkachocolate <Nyanyoka@yandex.ru>
2020-05-29 00:23:03 +0400

    all class + main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,122 @@
+#include "vector.h"
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  //тип - int, конструктор по умолчанию, методы size(), capacity(), push_back()
+  std::cout << "Тип - int, конструктор по умолчанию, методы size(), "
+               "capacity(), push_back()"
+            << std::endl;
+  vector<int> vec1;
+  vec1.push_back(10);
+  vec1.push_back(20);
+  vec1.print();
+  std::cout << "If size<=4: size = " << vec1.size()
+            << ", capacity = " << vec1.capacity() << std::endl;
+  for (size_t i = 3; i < 8; i++)
+    vec1.push_back(i * 10);
+  vec1.print();
+  std::cout << "If size>4: size = " << vec1.size()
+            << ", capacity = " << vec1.capacity() << std::endl;
+  std::cout << std::endl;
+
+  // тип - double, конструктор по умолчанию
+  std::cout << "Тип - double, конструктор по умолчанию" << std::endl;
+  vector<double> d1;
+  for (size_t i = 0; i < 6; i++)
+    d1.push_back(i * 0.1);
+  d1.print();
+  // тип - double, конструктор копирования
+  std::cout << "Тип - double, конструктор копирования от предыдущего вектора"
+            << std::endl;
+  vector<double> d2(d1);
+  d2.print();
+  std::cout << std::endl;
+
+  // оператор присваивания
+  std::cout << "Оператор присваивания (перед этим push_back(3.1416))" <<
+  std::endl;
+  d2.push_back(3.1416);
+  d1 = d2;
+  d1.print();
+  std::cout << std::endl;
+
+  // получение эелемента по позиции, первый и последний элементы
+  std::cout << "Получение эелемента по позиции" << std::endl;
+  try {
+    for (size_t i = 0; i <= d1.size(); i++)
+      std::cout << "Элемент №" << i << " - " << d1.at(i) << std::endl;
+  } catch (std::exception &d) {
+    std::cout << "Поймали выход за границы" << std::endl;
+    std::cout << d.what() << std::endl;
+  }
+  std::cout << "Первый элемент - " << d1.front() << std::endl;
+  std::cout << "Последний элемент - " << d1.back() << std::endl;
+  std::cout << std::endl;
+
+  // проверка метода empty() и clear() (после очищения вектор становится пустым)
+  std::cout << "Проверка метода empty() и clear() (после очищения вектор "
+               "становится пустым)"
+            << std::endl;
+  if (!d1.empty())
+    std::cout << "Данный массив не пустой" << std::endl;
+  d1.clear();
+  if (d1.empty())
+    std::cout << "Данный массив пустой после clear()" << std::endl;
+  std::cout << std::endl;
+
+  // вставка элементов
+  std::cout << "Вставка элементов в вектор:" << std::endl;
+  vec1.print();
+  vec1.insert(4, 2222);
+  std::cout << "Вектор должен быть после вставки на 4 позицию 2222:\n/10 20 "
+               "30 "
+               "40 2222 50 60 70/"
+            << std::endl;
+  vec1.print();
+  std::cout << std::endl;
+
+  // удавление элементов
+  std::cout << "Удаление элелемента в конце" << std::endl;
+  vec1.pop_back();
+  vec1.print();
+  std::cout << "Удаление с позиции 3" << std::endl;
+  vec1.erase(3);
+  std::cout << "Вектор должен быть после удаления:\n/10 "
+               "20 30 2222 50 60/"
+            << std::endl;
+  vec1.print();
+  std::cout << "size = " << vec1.size() << ", capacity = " << vec1.capacity()
+            << std::endl;
+  std::cout << "Удаление с позиций 1-2-3" << std::endl;
+  vec1.erase(1, 3);
+  std::cout << "Вектор должен быть после удаления:\n/10"
+               " 50 60/"
+            << std::endl;
+  vec1.print();
+  std::cout << "size = " << vec1.size() << ", capacity = " << vec1.capacity()
+            << ", то есть capacity "
+               "уменьшился"
+            << std::endl;
+  std::cout << std::endl;
+
+  // resize()
+  std::cout << "resize()" << std::endl;
+  std::cout << "До resize: size = " << d1.size()
+            << ", capacity = " << d1.capacity() << std::endl;
+  d1.resize(26);
+  std::cout << "После resize: size = " << d1.size()
+            << ", capacity = " << d1.capacity() << std::endl;
+  d1.clear();
+  d1.push_back(144.90);
+  std::cout << std::endl;
+
+  // swap()
+  std::cout << "swap()" << std::endl;
+  std::cout << "До swap" << std::endl;
+  d1.print();
+  d2.print();
+  std::cout << "После swap" << std::endl;
+  d1.swap(d2);
+  d1.print();
+  d2.print();
 }
--- vector.h
+++ vector.h
@@ -10,22 +10,64 @@ private:
   size_t size_ = 0;
   size_t capacity_ = 0;
 
-public:
-  explicit vector(size_t length) : size_(length) { data_ = new T[size_]; }
+  explicit vector(size_t length) : size_(length) {
+    capacity_ = cap(size_);
+    data_ = new T[capacity_];
+  }
 
+  //увеличение capacity
+  void IncreaseCapacity(const size_t &new_cap) {
+    capacity_ = cap(new_cap);
+    T *data1 = new T[capacity_];
+    std::copy(data_, data_ + size_, data1);
+    delete[] data_;
+    data_ = data1;
+  }
+
+  // вычисление capacity для значения size
+  size_t cap(const size_t &size) {
+    if (size <= 4)
+      return size;
+    else {
+      size_t cap = 4;
+      while (cap < size)
+        cap *= 1.5;
+      return cap;
+    }
+  }
+
+  void invalid_index(const size_t &pos) {
+    if (pos < 0 || pos >= size_)
+      throw std::out_of_range("Invalid index");
+  }
+
+public:
   vector() : vector(0) {}
 
-  vector(const vector<T> &other) : size_(other.Size()) {
-    std::copy(other, other + other.Size(), data_);
+  vector(const vector<T> &other) : vector(other.size_) {
+    std::copy(other.data_, other.data_ + size_, data_);
   }
 
   ~vector() { delete[] data_; }
 
-  [[nodiscard]] size_t Size() const noexcept { return size_; }
+  [[nodiscard]] size_t size() const noexcept { return size_; }
+
+  vector<T> &operator=(const vector<T> &other) {
+    if (capacity_ != other.capacity_) {
+      T *data1 = new T[other.capacity_];
+      delete[] data_;
+      data_ = data1;
+    }
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    std::copy(other.data_, other.data_ + other.size_, data_);
+    return *this;
+  }
+
+  T &operator[](const size_t &pos) { return data_[pos]; }
 
   T &at(const size_t &pos) {
-    if (pos < 0 || pos >= size_)
-      throw std::out_of_range("Invalid index");
+    invalid_index(pos);
     return data_[pos];
   }
 
@@ -37,6 +79,13 @@ public:
 
   bool empty() const noexcept { return size_ == 0; }
 
+  void reserve(size_t new_cap) {
+    // что-то резервируем толкьо если нужно увеличить capacity, исключение
+    // бросает выделение новой памяти (new)
+    if (capacity_ < new_cap)
+      IncreaseCapacity(new_cap);
+  }
+
   size_t capacity() const noexcept { return capacity_; }
 
   void clear() noexcept {
@@ -44,15 +93,91 @@ public:
     delete[] data_;
     data_ = data1;
     size_ = 0;
+    capacity_ = 0;
   }
 
   void insert(const size_t &pos, const T &value) {
-    T *data1 = new T[size_ + 1];
-    std::copy(data_, data_ + pos, data1);
-    data1[pos] = value;
-    std::copy(data_ + pos, data_ + size_, data1 + pos);
+    invalid_index(pos);
+    if (size_ == capacity_)
+      IncreaseCapacity(capacity_ + 1);
+    for (size_t i = size_; i > pos; i--)
+      data_[i] = data_[i - 1]; // size_ != 0, т.к. мы предварительно увеличили
+    // массив
+    data_[pos] = value;
+    size_++;
+  }
+
+  void push_back(const T &value) {
+    if (size_ == capacity_)
+      IncreaseCapacity(capacity_ + 1);
+    data_[size_] = value;
+    size_++;
+  }
+
+  void erase(const size_t &pos) {
+    invalid_index(pos);
+    for (size_t i = pos; i < size_ - 1; ++i)
+      data_[i] = data_[i + 1];
+    size_--;
+    if (capacity_ != cap(size_)) {
+      capacity_ = cap(size_ - 1);
+      T *data1 = new T[capacity_];
+      std::copy(data_, data_ + size_, data1);
+      delete[] data_;
+      data_ = data1;
+    }
+  }
+
+  void erase(const size_t &pos1, const size_t &pos2) {
+    invalid_index(pos1);
+    invalid_index(pos2);
+    if (pos1 > pos2)
+      throw std::out_of_range(
+          "Invalid index: first should be less than the second");
+    else {
+      for (size_t i = pos1; i < size_ - (pos2 - pos1) - 1; ++i)
+        data_[i] = data_[i + pos2 - pos1 + 1];
+      size_ -= pos2 - pos1 + 1;
+      if (capacity_ != cap(size_)) {
+        capacity_ = cap(size_);
+        T *data1 = new T[capacity_];
+        std::copy(data_, data_ + size_, data1);
+        delete[] data_;
+        data_ = data1;
+      }
+    }
+  }
+
+  void pop_back() {
+    capacity_ = cap(size_ - 1);
+    T *data1 = new T[capacity_];
+    std::copy(data_, data_ + size_ - 1, data1);
     delete[] data_;
     data_ = data1;
+    size_--;
+  }
+
+  void resize(const size_t &new_size) {
+    size_ = new_size;
+    if (capacity_ != cap(size_)) {
+      capacity_ = cap(size_);
+      T *data1 = new T[capacity_];
+      std::copy(data_, data_ + size_, data1);
+      delete[] data_;
+      data_ = data1;
+    }
+  }
+
+  void swap(vector<T> &other) {
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+  }
+
+  void print() const {
+    for (size_t i = 0; i < size_; i++)
+      std::cout << data_[i] << ' ';
+    std::cout << std::endl;
   }
 };
 
